59415fbb4a7f87b77882b0c962c3f01b57d9b68c
Gaurav <bhardwajgaurav204@gmail.com>
2025-07-18 01:21:41 +0530

    portfolio bug fixing on deployment

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,3 +1,4 @@
+'use client';
 export default function Footer() {
     return (
       <footer className="py-6 text-center text-gray-500 dark:text-gray-400 border-t dark:border-gray-700 mt-10">
--- src/app/components/Loader.js
+++ src/app/components/Loader.js
@@ -1,4 +1,5 @@
 // components/Loader.jsx
+'use client';
 export default function Loader() {
   return (
     <div className="fixed inset-0 z-50 bg-black flex items-center justify-center">
